31a57fc9c60c1f0cda0c0d7d8859eec5673d3c4c
Sweetlana Protsenko <svitprotsenko@gmail.com>
2022-02-17 23:03:27 -0700

    Velidation Form

--- validate.js
+++ validate.js
@@ -1,13 +1,32 @@
 
-var phoneNumber = document.getElementById("phoneNumber");
-var x = document.forms["contactForm"]["name"].value;
-var text;
 function validate() {
-    if (x == "") {
-        text = "Name must be filled out";
-    } else {
-        text = "Information is valid";
+    
+    var name = document.forms["contactForm"]["name"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var phone = document.forms["contactForm"]["phoneNumber"].value;
+    var phoneno = "/^[0123456789.-]+$/";
+    var mail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    
+
+    if (name != "") {
+        document.getElementById("nameInfo").innerText = "Name is ok";
+        return false;
     }
-    document.getElementById("demo").innerHTML = text;    
+    
+
+    if (phone.match(phoneno)) {
+        document.getElementById("phoneInfo").innerText = "Phone is ok";
+        return false;
+    } else {
+        alert("Invalid input");
+    }
+    
+    if (email.match(mail)) {
+        document.getElementById("emailInfo").innerHTML = "Email is ok";
+        return true;
+    } else {
+        alert("incorrect");
+        return false;
+    }
     
 }
